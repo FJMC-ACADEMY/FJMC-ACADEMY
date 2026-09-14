cb7b31a89b9e8a962b3be836c204b5d27ebaab51
fjmcacademy <fjmcacademy1008@gmail.com>
2026-09-14 23:01:48 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,46 @@
+import { auth } from "./firebase.js";
+
+import {
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+
+const form = document.getElementById("loginForm");
+
+const message = document.getElementById("loginMessage");
+
+
+form.addEventListener("submit", async function(e) {
+
+    e.preventDefault();
+
+    const email =
+        document.getElementById("email").value.trim();
+
+    const password =
+        document.getElementById("password").value;
+
+
+    try {
+
+        await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        window.location.href =
+            "dashboard.html";
+
+    }
+
+    catch(error) {
+
+        console.error(error);
+
+        message.innerText =
+            "Invalid email or password.";
+
+    }
+
+});
